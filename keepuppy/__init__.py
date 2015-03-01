62671e3af2be81c5ea992e51473c0e08e939b5d7
Warren Moore <warren@wamonite.com>
2015-03-01 15:28:51 +0000

    Bump version: 1.0.7 → 1.0.8

--- keepuppy/__init__.py
+++ keepuppy/__init__.py
@@ -5,7 +5,7 @@ keepuppy
 """
 
 __title__ = 'keepuppy'
-__version__ = '1.0.7'
+__version__ = '1.0.8'
 __author__ = 'Warren Moore'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2014 Warren Moore'
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name = 'keepuppy',
-    version = '1.0.7',
+    version = '1.0.8',
     description = 'KeePass database SFTP sync.',
     long_description = readme,
     license = 'MIT',
