2e7f099676bab447908e71c95c7a28884c5a215f
requires.io <support@requires.io>
2017-11-15 00:17:37 +0000

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         'keepuppy': ['data/com.wamonite.keepuppy.plist']
     },
     scripts = ['keepuppy_sync.py', 'keepuppy_restart.py'],
-    install_requires = ['paramiko==2.3.1', 'psutil==5.4.1'],
+    install_requires = ['paramiko==2.4.0', 'psutil==5.4.1'],
     setup_requires = ['nose==1.3.7'],
     tests_require = ['sftpserver==0.3', 'mock==2.0.0'],
     zip_safe = False
